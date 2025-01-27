10bcb015723aac3e1865581c5f465a958fcb8cb3
Christen <christen.srghaly@yahoo.com>
2025-01-27 17:05:55 -0500

    Modify models

--- wioblapp/models.py
+++ wioblapp/models.py
@@ -1,51 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Account(models.Model):
-    username = models.CharField("Username", max_length=100)
-    email = models.EmailField("Email Adress", max_length=254, unique=True)
-    password = models.CharField("Password", max_length=100)
+class Role(models.Model):
+    name = models.CharField("Role", max_length=100)
+    description = models.TextField("Role Description")
+
+    def __str__(self):
+        return self.name
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_accounts")
+    role = models.ForeignKey(Role, on_delete=models.RESTRICT, related_name="user_accounts")
+    bio = models.TextField("Bio")
+    # username = models.CharField("Username", max_length=100)
+    # email = models.EmailField("Email Adress", max_length=254, unique=True)
+    # password = models.CharField("Password", max_length=100)
 
     def __str__(self):
         return self.username
 
-class Admin(Account):
-    pass
-    # account = models.OneToOneField(Account, on_delete=models.CASCADE, related_name="admin")
-
-class Coach(Account):
-    details = models.TextField("Details")
-    # account = models.OneToOneField(Account, on_delete=models.CASCADE, related_name="coach")
-
-    def __str__(self):
-        return self.account.username
-
 class Team(models.Model):
     name = models.CharField("Team Name", max_length=20)
-    coaches = models.ManyToManyField(Coach, related_name="team")
+    coaches = models.ManyToManyField(UserAccount, related_name="teams")
     place = models.IntegerField("Place", default=0)
 
     def __str__(self):
         return self.name
 
-class Registeration(models.Model):
+class Registration(models.Model):
     title = models.CharField("Title", max_length=50)
     description = models.CharField("Description", max_length=300)
-    amount = models.IntegerField("Amount")
+    cost = models.IntegerField("Cost")
 
     def __str__(self):
         return self.title
 
-class Parent(Account):
-    balance = models.IntegerField("Balance", default=0)
-    # account = models.OneToOneField(Account, on_delete=models.CASCADE, related_name="parent")
-
-    def __str__(self):
-        return self.account.username
-
 class Player(models.Model):
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name="player")
-    team = models.ForeignKey(Team, on_delete=models.RESTRICT, related_name="player")
-    registeration = models.ForeignKey(Registeration, on_delete=models.CASCADE, related_name="player")
+    related_account = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name="players")
+    team = models.ForeignKey(Team, on_delete=models.RESTRICT, related_name="players")
+    registration = models.ForeignKey(Registration, on_delete=models.CASCADE, related_name="players")
     name = models.CharField("Name", max_length=50)
     dob = models.DateField("Date Of Birth")
 
@@ -60,19 +53,20 @@ class Park(models.Model):
         return self.name
 
 class Game(models.Model):
-    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="game")
-    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="game")
-    park = models.ForeignKey(Park, on_delete=models.CASCADE, related_name="game")
-    winner = models.ForeignKey(Team, on_delete=models.RESTRICT, related_name="game", blank=True, null=True)
+    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="Team1_games")
+    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="Team2_games")
+    park = models.ForeignKey(Park, on_delete=models.CASCADE, related_name="games")
+    winner = models.ForeignKey(Team, on_delete=models.RESTRICT, related_name="games", blank=True, null=True)
     date_time = models.DateTimeField("Date")
-    score = models.CharField("Final Score", blank=True, null=True)
+    team1_score = models.IntegerField("Team #1 Score", blank=True, null=True)
+    team2_score = models.IntegerField("Team #2 Score", blank=True, null=True)
 
     def __str__(self):
         return f"{self.team_1} / {self.team_2}"
 
 class Comment(models.Model):
-    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="comment")
-    parent = models.ForeignKey(Parent, on_delete=models.RESTRICT, related_name="comment")
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="comments")
+    user_account = models.ForeignKey(UserAccount, on_delete=models.RESTRICT, related_name="comments")
     content = models.TextField("Content")
     date = models.DateTimeField("Date", auto_now_add=True)
 
@@ -80,7 +74,7 @@ class Comment(models.Model):
         return self.content[:20]
 
 class Announcement(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.RESTRICT, related_name="announcement")
+    user_account = models.ForeignKey(UserAccount, on_delete=models.RESTRICT, related_name="announcements")
     title = models.CharField("Title", max_length=100)
     content = models.TextField("Content")
     date = models.DateTimeField("Date", auto_now_add=True)
@@ -89,11 +83,10 @@ class Announcement(models.Model):
         return self.title
 
 class Flag(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.RESTRICT, related_name="flag")
-    flaged_content = models.CharField("Flaged Content", max_length=100)
-    value = models.BooleanField(default=False)
+    user_account = models.ForeignKey(UserAccount, on_delete=models.RESTRICT, related_name="flags")
+    flagged_content = models.CharField("Flagged Content", max_length=100)
     date = models.DateTimeField("Date", auto_now_add=True)
 
     def __str__(self):
-        return self.flaged_content
+        return self.flagged_content
 
