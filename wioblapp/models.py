a5cf3da5c8e39e8c87b93754bae8c2cf6cd52843
Christen <christen.srghaly@yahoo.com>
2025-01-28 14:48:02 -0500

    Modify models

--- wioblapp/models.py
+++ wioblapp/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Role(models.Model):
-    name = models.CharField("Role", max_length=100)
+    name = models.CharField("Role", max_length=100, primary_key=True)
     description = models.TextField("Role Description")
 
     def __str__(self):
         return self.name
 
-class UserAccount(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_accounts")
+class UserAccount(User):
     role = models.ForeignKey(Role, on_delete=models.RESTRICT, related_name="user_accounts")
     bio = models.TextField("Bio")
     # username = models.CharField("Username", max_length=100)
@@ -20,30 +19,37 @@ class UserAccount(models.Model):
         return self.username
 
 class Team(models.Model):
-    name = models.CharField("Team Name", max_length=20)
-    coaches = models.ManyToManyField(UserAccount, related_name="teams")
+    name = models.CharField("Team Name", max_length=20, primary_key=True)
+    coaches = models.ManyToManyField(UserAccount, on_delete=models.RESTRICT, related_name="teams")
     place = models.IntegerField("Place", default=0)
 
     def __str__(self):
         return self.name
 
-class Registration(models.Model):
-    title = models.CharField("Title", max_length=50)
-    description = models.CharField("Description", max_length=300)
-    cost = models.IntegerField("Cost")
-
-    def __str__(self):
-        return self.title
-
 class Player(models.Model):
     related_account = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name="players")
-    team = models.ForeignKey(Team, on_delete=models.RESTRICT, related_name="players")
-    registration = models.ForeignKey(Registration, on_delete=models.CASCADE, related_name="players")
+    team_name = models.ForeignKey(Team, on_delete=models.RESTRICT, related_name="players")
     name = models.CharField("Name", max_length=50)
     dob = models.DateField("Date Of Birth")
 
     def __str__(self):
         return self.name
+
+class RegistrationTypes(models.Model):
+    reg_type = models.CharField("Registration Type", max_length=50, primary_key=True)
+    description = models.CharField("Description", max_length=300)
+    cost = models.IntegerField("Cost")
+
+    def __str__(self):
+        return self.reg_type
+
+class Registration(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="registrations")
+    reg_type = models.ForeignKey(RegistrationTypes, on_delete=models.CASCADE, related_name="registrations")
+    date_time = models.DateTimeField("Date", auto_now_add=True)
+
+    def __str__(self):
+        return self.reg_type
 
 class Park(models.Model):
     name = models.CharField("Park Name", max_length=100)
