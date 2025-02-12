2b86e55748e33077f6a72d4902c0e0d537ac21b7
Molly Luis <mollyluis555@gmail.com>
2025-02-12 16:00:46 -0500

    Resolved conflict by keeping the database from the other branch

--- wioblapp/views.py
+++ wioblapp/views.py
@@ -257,37 +257,11 @@ def teams(request):
 # --------------------------------------------------------------
 
 def team_schedule(request, team_name):
-    schedule_form = TeamScheduleForm(request.GET)
+    schedule_form = TeamScheduleForm()
     team = team_name
-    games = ( Game.objects.filter(team_1=team) | Game.objects.filter(team_2=team) ).distinct()
-
-    if schedule_form.is_valid():
-        month = schedule_form.cleaned_data.get('month')
-        date = schedule_form.cleaned_data.get('date')
-        result = schedule_form.cleaned_data.get('result')
-
-        if month:
-            if month == "All":
-                games = ( Game.objects.filter(team_1=team) | Game.objects.filter(team_2=team) ).distinct()
-            else:
-                games = games.filter(date_time__month=int(month))
-
-        if date:
-            if date == "Ascending":
-                games = games.order_by("-date_time")
-            elif date == "Descending":
-                games = games.order_by("date_time")
-
-        if result:
-            if result == "Win":
-                games = games.filter(winner=team)
-            elif result == "Lose":
-                games = games.exclude(winner=team)
-
     context = {
         "schedule_form": schedule_form,
         "team": team,
-        "games": games,
     }
     return render(request, "team_schedule.html", context)
 
