9a4c892a3721255ab99f098dfbb981e9467c764a
Christen <christen.srghaly@yahoo.com>
2025-02-13 16:58:08 -0500

    Add create, like, flag comment

--- wioblapp/urls.py
+++ wioblapp/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path("registration/", views.register_player, name="registration"),
     path("teams/", views.teams, name="teams"),
     path("team_schedule/<str:team_name>/", views.team_schedule, name="team_schedule"),
+    path("create-comment/<str:team_name>/", views.create_comment, name="create_comment"),
+    path("like-comment/<str:team_name>/", views.like_comment, name="like_comment"),
+    path("flag-comment/<str:team_name>/", views.flag_comment, name="flag_comment"),
 ]
