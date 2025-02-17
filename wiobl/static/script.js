ff71227d0d53d243ea370bdee1e60609a07539c3
2003480LM <134742988+2003480LM@users.noreply.github.com>
2025-02-16 19:21:11 -0500

    made changes to the API and the footer

--- wiobl/static/script.js
+++ wiobl/static/script.js
@@ -29,27 +29,48 @@ document.addEventListener("DOMContentLoaded", () => {
     handleBurgerClick();
 });
 
-// Run `toggleBurger` whenever the window is resized
+// Run ⁠ toggleBurger ⁠ whenever the window is resized
 window.addEventListener("resize", toggleBurger);
 
 
 // map api
 document.addEventListener('DOMContentLoaded', function () {
-    const coordinates = [45.46120974762705, -73.80929962254508];
-    const locationName = 'Lakeside Heights, Pointe-Claire, QC';
+    const coordinates_court_1 = [45.46120974762705, -73.80929962254508];//Pointe Claire Park
+    const coordinates_court_2 = [45.48934050768304, -73.85824621609802];//Pierrefonds-Roxboro Park
+    const coordinates_court_3 = [45.48272170531596, -73.8067795278847];//Dollard-Ddes-Ormeaux Park
+    const coordinates_court_4 = [45.44387748895584, -73.8607540133765];//Kirkland Park
+    const locationName_1 = 'Lakeside Heights, Pointe-Claire, QC';
+    const locationName_2 = 'Parc Ménard, Pierrefonds-Roxboro, QC';
+    const locationName_3 = 'Dollard-Ddes-Ormeaux, QC';
+    const locationName_4 = 'Kirkland, QC';
 
     const map = L.map('map', {
         layers: [
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                'attribution': 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+                'attribution': 'Map data ©️ <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
             })
         ],
-        center: coordinates,
-        zoom: 15
+        center: coordinates_court_1,
+        zoom: 12
     });
 
-    const marker = L.marker(coordinates)
-    .addTo(map)
-    .bindPopup(locationName)
-    .openPopup();
+    const marker_1 = L.marker(coordinates_court_1)
+        .addTo(map)
+        .bindPopup(locationName_1)
+        .openPopup();
+
+    const marker_2 = L.marker(coordinates_court_2)
+        .addTo(map)
+        .bindPopup(locationName_2)
+        .openPopup();
+
+    const marker_3 = L.marker(coordinates_court_3)
+        .addTo(map)
+        .bindPopup(locationName_3)
+        .openPopup();
+
+    const marker_4 = L.marker(coordinates_court_4)
+        .addTo(map)
+        .bindPopup(locationName_4)
+        .openPopup();
 });
