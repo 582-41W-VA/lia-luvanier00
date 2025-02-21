41cc4b9f8fe0ef1c285d85626822983ca74362ee
Molly Luis <mollyluis555@gmail.com>
2025-02-21 11:08:44 -0500

    add all the methods

--- wioblapp/admin.py
+++ wioblapp/admin.py
@@ -125,7 +125,12 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ("date",)  
     ordering = ("likes",) 
     search_fields = ("user_account__username", )
+    exclude = ("user_account", "likes",)
 
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.user_account = request.user
+        super().save_model(request, obj, form, change)
 
 
     def get_ordering(self, request):
@@ -150,6 +155,14 @@ class FavoriteTeamAdmin(admin.ModelAdmin):
     list_display = ("user_account", "team") 
     list_filter = ("team",)  
     search_fields = ("user_account__username", "team__name")
+    exclude = ("user_account", )
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.user_account = request.user
+        super().save_model(request, obj, form, change)
+
+
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ("team_1", "team_2", "date_time", "winner", "team1_score", "team2_score", "park")
@@ -163,6 +176,14 @@ class LikedCommentAdmin(admin.ModelAdmin):
     list_filter = ("user_account",) 
     search_fields = ("user_account__username", "comment__content")  
     ordering = ("-id",) 
+    exclude = ("user_account", )
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.user_account = request.user
+        super().save_model(request, obj, form, change)
+
+
 
     def comment_preview(self, obj):
         return obj.comment.content[:50] + "..." if len(obj.comment.content) > 50 else obj.comment.content
@@ -175,18 +196,52 @@ class ParkAdmin(admin.ModelAdmin):
     list_filter = ("name",)  
     ordering = ("name",) 
 
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("name", "related_account", "team_name", "dob", "gender") 
+    search_fields = ("name", "related_account__username", "team_name__name")  
+    list_filter = ("team_name", "gender", "dob") 
+    ordering = ("name",) 
+
+class RegistrationTypeAdmin(admin.ModelAdmin):
+    list_display = ("reg_type", "description", "cost") 
+    search_fields = ("reg_type", "description") 
+    list_filter = ("cost",) 
+    ordering = ("reg_type",) 
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")  
+    search_fields = ("name", "description") 
+    ordering = ("name",) 
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_coaches", "place", "group", "color") 
+    search_fields = ("name", "coaches__username", "group__reg_type") 
+    list_filter = ("group", "place")
+    ordering = ("place", "name")  
+
+    def get_coaches(self, obj):
+        """Show a list of coaches assigned to the team."""
+        return ", ".join([coach.username for coach in obj.coaches.all()]) if obj.coaches.exists() else "No Coaches"
+
+    get_coaches.short_description = "Coaches"
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "role", "phone", "is_active", "is_staff")  
+    search_fields = ("username", "email", "role__name", "phone") 
+    list_filter = ("role", "is_active", "is_staff", "is_superuser")  
+    ordering = ("username",)  
 
 
 wiobl_site = WioblAdminArea(name='WioblAdmin')
 
 
 
-wiobl_site.register(Role)
-wiobl_site.register(UserAccount)
-wiobl_site.register(Team)
+wiobl_site.register(Role, RoleAdmin)
+wiobl_site.register(UserAccount, UserAccountAdmin)
+wiobl_site.register(Team, TeamAdmin)
 wiobl_site.register(FavoriteTeam, FavoriteTeamAdmin)
-wiobl_site.register(Player)
-wiobl_site.register(RegistrationType)
+wiobl_site.register(Player, PlayerAdmin)
+wiobl_site.register(RegistrationType, RegistrationTypeAdmin)
 wiobl_site.register(Registration, RegistrationAdmin)
 wiobl_site.register(Park, ParkAdmin)
 wiobl_site.register(Game, GameAdmin)
